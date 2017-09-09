d40f70c51e0e192dc4415854f1ccdd70c6bbec5b
Nikita Yunoshev <yunoshev.nr@gmail.com>
2017-09-09 20:26:37 +0300

    Ready for deploy

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
     </div>
     <div class="form-group">
         <h5>Размер матрицы(X)</h5>
-        <input type="number" class="form-control" id="edge" placeholder="number from 1 to 100">
+        <input type="number" class="form-control" id="edge" placeholder="number from 1 to 100" value="25">
     </div>
     <div class="form-group" class="form-control">
         <h5>Разраженность матрицы(R)</h5>
-        <input type="number" class="form-control" id="rarefaction" placeholder="number from 1 to 100">
+        <input type="number" class="form-control" id="rarefaction" placeholder="number from 1 to 100" value="30">
     </div>
     <div class="form-group">
         <h5>Алгоритм поиска</h5>
@@ -32,19 +32,20 @@
     </div>
     <div class="form-group">
         <h5>Скорость анаманции(А)</h5>
-        <input type="number" class="form-control" placeholder="number from 100 to 1000(milliseconds)" id="animation_speed">
+        <input type="number" class="form-control" placeholder="number from 100 to 1000(milliseconds)" value="100"
+               id="animation_speed">
     </div>
     <div class="form-group">
         <h5>Координата начальной позиции маркера в матрице (START)</h5>
-        <input type="text" class="form-control" id="start-coordinat" placeholder="Row, Col">
+        <input type="text" class="form-control" id="start-coordinat" placeholder="Row, Col" value="25,1">
     </div>
     <div class="form-group">
         <h5>Координата конечной позиции маркера в матрице (END)</h5>
-        <input type="text" class="form-control" id="end-coordinat" placeholder="Row, Col">
+        <input type="text" class="form-control" id="end-coordinat" placeholder="Row, Col" value="1,25">
     </div>
     <div class="form-group">
         <h5>Метод перехода(М)</h5>
-        <select name="transition_method" class="form-control" id = "transition_method">
+        <select name="transition_method" class="form-control" id="transition_method">
             <option value="four_steps">4 steps</option>
             <option value="eight_steps">8 steps</option>
         </select>
@@ -57,13 +58,12 @@
             <option value="randomly">Randomly</option>
         </select>
     </div>
-    <button class="btn btn-default start-search">Начать поиск</button>
+    <button class="button start-search">Начать поиск</button>
 </div>
 <div class="table">
     <table class="matrix"></table>
-    <button class="btn btn-success continue-search">Продолжить поиск</button>
+    <button class="button continue-search">Продолжить поиск</button>
 </div>
-
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -71,16 +71,13 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title" id="myModalLabel">Ошибка</h4>
             </div>
-            <div class="modal-body" id ="modal-body">
-
-            </div>
+            <div class="modal-body" id="modal-body"></div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
             </div>
         </div>
     </div>
 </div>
-
 <script src="../js/bootstrap.min.js"></script>
 <script src="js/script.js"></script>
 </body>
